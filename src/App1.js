900659e5bb8b9633609722454e92b937e8b554b9
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-09-23 17:53:21 +0100

    get places id

--- src/App1.js
+++ src/App1.js
@@ -1,10 +1,21 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import React, {
+  useState,
+  useCallback,
+  useEffect
+} from 'react';
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow
+} from '@react-google-maps/api';
 import axios from 'axios';
 import uuid from 'react-uuid';
 import Rating from "@material-ui/lab/Rating";
 
-import { usePosition } from './Hooks/usePosition';
+import {
+  usePosition
+} from './Hooks/usePosition';
 import locations from './nearByRestaurants';
 import AddRestaurant from './components/AddRestaurant';
 import Restaurant from './Restaurant';
@@ -12,7 +23,7 @@ import Restaurant from './Restaurant';
 
 const libraries = ["places"]
 const mapContainerStyle = {
-  width: '80vw',
+  width: '60vw',
   height: '80vh',
   position: 'relative'
 }
@@ -23,10 +34,13 @@ const options = {
 }
 
 const App1 = () => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries
-  })
+  const {
+      isLoaded,
+    loadError
+    } = useLoadScript({
+      googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+      libraries
+    })
 
   const [currentPosition, setCurrentPosition] = useState({});
   // Check if a location is clicked on the Map
@@ -87,7 +101,7 @@ const App1 = () => {
   useEffect(() => {
     axios
       .get(
-      `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${currentPosition.lat},${currentPosition.lng}&radius=2000&type=restaurant&key=AIzaSyAKrsTZlN-C-eEq1D8nEwtSDnDd9xtknkI`
+      `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${currentPosition.lat},${currentPosition.lng}&radius=2000&type=restaurant&key=AIzaSyAMDtC9Z6uMrTV_NsWjjdeskdGE5W-hITY`
       )
       .then((res) => {
         let Feeds = res.data.results;
@@ -100,57 +114,76 @@ const App1 = () => {
   if (loadError) return 'Error loading map'
   if (!isLoaded) return 'Loading Maps'
 
-  return <div className="map-section">
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
+  return <div className="map-section" >
+    <GoogleMap mapContainerStyle={mapContainerStyle}
       zoom={16}
       center={currentPosition}
-      options={options}
-      onClick={onMapClick}
-
-    >
-      {feeds.map((foodPlace, index) => {
-        // Creating a position object using the lat and lng from the Places API
-        const position = {
-          lat: foodPlace.geometry.location.lat,
-          lng: foodPlace.geometry.location.lng
-        };
-        return foodPlace.rating <= filterRating.filterRatingValue ? (<Marker
-          icon={{
-            path:
-              "M7 0c-3.314 0-6 3.134-6 7 0 3.31 1.969 6.083 4.616 6.812l-0.993 16.191c-0.067 1.098 0.778 1.996 1.878 1.996h1c1.1 0 1.945-0.898 1.878-1.996l-0.993-16.191c2.646-0.729 4.616-3.502 4.616-6.812 0-3.866-2.686-7-6-7zM27.167 0l-1.667 10h-1.25l-0.833-10h-0.833l-0.833 10h-1.25l-1.667-10h-0.833v13c0 0.552 0.448 1 1 1h2.604l-0.982 16.004c-0.067 1.098 0.778 1.996 1.878 1.996h1c1.1 0 1.945-0.898 1.878-1.996l-0.982-16.004h2.604c0.552 0 1-0.448 1-1v-13h-0.833z",
-            fillColor: "#375fc4",
-            fillOpacity: 1.0,
-            strokeWeight: 0,
-            scale: 1,
-          }}
-          key={index} onClick={() => console.log("I have been clicked")} position={position} />) : null
+      options={
+        options
       }
-      )
-      }
+      onClick={onMapClick}>
       {
-        currentPosition.lat && (<Marker onClick={() => console.log("I have been clicked")}
-          icon={"https://maps.google.com/mapfiles/ms/icons/blue-dot.png"}
-          position={currentPosition}
+        feeds.map((foodPlace, index) => {
+          // Creating a position object using the lat and lng from the Places API
+          const position = {
+            lat: foodPlace.geometry.location.lat,
+            lng: foodPlace.geometry.location.lng
+          };
+          return foodPlace.rating <= filterRating.filterRatingValue ? (< Marker icon={
+            {
+              path: "M7 0c-3.314 0-6 3.134-6 7 0 3.31 1.969 6.083 4.616 6.812l-0.993 16.191c-0.067 1.098 0.778 1.996 1.878 1.996h1c1.1 0 1.945-0.898 1.878-1.996l-0.993-16.191c2.646-0.729 4.616-3.502 4.616-6.812 0-3.866-2.686-7-6-7zM27.167 0l-1.667 10h-1.25l-0.833-10h-0.833l-0.833 10h-1.25l-1.667-10h-0.833v13c0 0.552 0.448 1 1 1h2.604l-0.982 16.004c-0.067 1.098 0.778 1.996 1.878 1.996h1c1.1 0 1.945-0.898 1.878-1.996l-0.982-16.004h2.604c0.552 0 1-0.448 1-1v-13h-0.833z",
+              fillColor: "#375fc4",
+              fillOpacity: 1.0,
+              strokeWeight: 0,
+              scale: 1,
+            }
+          }
+            key={index}
+            onClick={() => console.log("I have been clicked")}
+            position={position}
+          />) : null
+        }
+        )
+      } {
+        currentPosition.lat && (< Marker onClick={
+          () => console.log("I have been clicked")
+        }
+          icon={
+            "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+          }
+          position={
+            currentPosition
+          }
         />)
       }
+      {
+        selected ? (< InfoWindow position={
+          {
+            lat: lat,
+            lng: lng
+          }
+        }>
+          <AddRestaurant feeds={feeds}
+            setFeeds={setFeeds}
+            location={
+              { lat, lng }
+            }
+          />
 
-
-      {selected ? (<InfoWindow position={{ lat: lat, lng: lng }}><AddRestaurant feeds={feeds} setFeeds={setFeeds} location={{ lat, lng }} />
-
-      </InfoWindow>) : null}
+        </InfoWindow>) : null}
 
     </GoogleMap>
     <div>
       <>
-      <label htmlFor='restaurants'>Filter by Review:</label>
-      <Rating
-        name="half-rating"
-        precision={0.5}
-        value={filterRating.filterRatingValue}
-        onChange={onFilterRatingChange}
-      />
-      <Restaurant feeds={feeds} filterRating={filterRating.filterRatingValue} />
+      <label htmlFor='restaurants'> Filter by Review: </label>
+      <Rating name="half-rating"
+        precision={
+          0.5
+        }
+        value={
+          filterRating.filterRatingValue
+        }
+        onChange={onFilterRatingChange} /> < Restaurant feeds={feeds} filterRating={filterRating.filterRatingValue} />
 
       </>
     </div>
--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Rating from '@material-ui/lab/Rating';
 import expand from './assets/expand.svg';
-const Restaurant = (props) => {
+import axios from 'axios';
+import styled from "styled-components";
+
+
+const Restaurant = ({ feeds, filterRating }) => {
+  const [toggle, toggleButton] = useState(false);
+  const [review, setReview] = useState([])
+
+  const handleDropDown = (place_id) => {
+    console.log(place_id)
+    toggleButton(!toggle)
+    getPlaceDetails(place_id);
+  }
+
+  const getPlaceDetails = ((place_id) => {
+    axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=name,rating,reviews,formatted_phone_number&key=AIzaSyAMDtC9Z6uMrTV_NsWjjdeskdGE5W-hITY`)
+      .then(
+      (response) => {
+        let placesResult = response.data.result.reviews;
+        console.log(placesResult)
+        setReview(placesResult)
+      })
+
+
+  })
   return (
 
     <div>
-      {props.feeds.map((feed, index) =>
-        feed.rating <= props.filterRating ? (
-          <div key={index}>
-            <img src={feed.icon} />
+      {feeds.map((feed, index) =>
+        feed.rating <= filterRating ? (
+          <Aside key={index}>
+            <Image src={feed.icon} />
             <div>
               <h2>{feed.name}</h2>
-              <p>{feed.vicinity.substring(0, 40)}</p>
+              <p>{feed.vicinity.length >= 30 ? feed.vicinity.substring(0, 30) : feed.vicinity}</p>
               <small>{feed.open_now}</small>
               <Rating
                 name="half-rating"
@@ -21,9 +45,17 @@ const Restaurant = (props) => {
                 size="small"
                 readOnly
               />
-              <img src={expand} onClick={(event) => console.log("I have been clicked")} />
+              <Icon src={expand} onClick={() => handleDropDown(feed.place_id)} />
+              {toggle ? (review.map((placeReview, index) => {
+                return <div key={index}>
+                  <h3>{placeReview.author_name}</h3>
+                  <h3>{placeReview.author_url}</h3>
+                  <p>{placeReview.text}</p>
+                  <small>{placeReview.rating}</small>
+                </div>
+              })) : null}
             </div>
-          </div>
+          </Aside>
         ) : (<span key={index}></span>))
 
       }
@@ -33,3 +65,29 @@ const Restaurant = (props) => {
 }
 
 export default Restaurant;
+
+const Aside = styled.aside`
+width:100%;
+height:auto;
+background:#f7f7f7;
+padding:0.5rem;
+
+h2{
+  font - weight: 700;
+  font-size:1rem;
+  margin:5px 0;
+  color:#222;
+}
+p{
+  font-size: 0.8rem;
+  font-weight:500;
+  margin:4px 0;
+}
+`
+const Image = styled.img`
+width:20%;
+`
+
+const Icon = styled.img`
+width:10%;
+`
