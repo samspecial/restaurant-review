6d0c6bd4e6ba7ad6d589dcd77ccf9914f8ab8c16
Samuel Osinloye <35423225+samspecial@users.noreply.github.com>
2021-03-08 04:38:17 +0100

    Update Restaurant.js

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -14,7 +14,7 @@ const Restaurant = ({ feed }) => {
   const [allReviews, setReview] = useState([]);
 
   const getPlaceDetails = ((place_id) => {
-    axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=name,rating,reviews,formatted_phone_number&key=AIzaSyAMDtC9Z6uMrTV_NsWjjdeskdGE5W-hITY`)
+    axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=name,rating,reviews,formatted_phone_number&key=AIzaSyAqOUWrdqXxfz82KxzEh8ef_RhUFai5od8`)
       .then(
       (response) => {
         let placesResult = response.data.result.reviews;
@@ -24,7 +24,7 @@ const Restaurant = ({ feed }) => {
 
   // Get StreetView Static Image
   const getStreetViewImage = (lat, lng) => {
-    axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/streetview?size=600x300&location=${lat},${lng}&heading=151.78&pitch=-0.76&key=key=AIzaSyAMDtC9Z6uMrTV_NsWjjdeskdGE5W-hITY&signature=YOUR_SIGNATURE`)
+    axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/streetview?size=600x300&location=${lat},${lng}&heading=151.78&pitch=-0.76&key=key=AIzaSyAqOUWrdqXxfz82KxzEh8ef_RhUFai5od8&signature=YOUR_SIGNATURE`)
       .then(
       (response) => {
         let streetViewImage = response.data;
